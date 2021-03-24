98d842d74943e2063bdad9328e5e916b99f430ea
KLIJIN <mdu091008@yandex.ru>
2021-03-25 02:22:17 +0700

    first commit

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ const camSliderOne = document.querySelector(".cam__sliderOne")
 const camSliderTwo = document.querySelector(".cam__sliderTwo")
 const camClose = document.querySelector(".cam__close")
 const camCloseR = document.querySelector(".cam__close--right")
+const form = document.querySelector(".formTel__form")
+
+
+camClose.addEventListener("click", () => closeBar())
+camCloseR.addEventListener("click", () => closeBar())
+
+const closeBar = () => {
+  modalBtn.forEach(item => item.style.display = "block")
+  camSliderOne.style.display = "none";
+  camSliderTwo.style.display = "none";
+}
 
 modalBtn.forEach(item => {
   item.addEventListener("click", event => {
@@ -21,8 +32,6 @@ modalBtn.forEach(item => {
   })
 })
 
-camClose.addEventListener("click", () => closeBar())
-camCloseR.addEventListener("click", () => closeBar())
 
 let handler = () => {
   (document.addEventListener("click", event => {
@@ -45,8 +54,32 @@ let handler = () => {
 }
 
 
-const closeBar = () => {
-  modalBtn.forEach(item => item.style.display = "block")
-  camSliderOne.style.display = "none";
-  camSliderTwo.style.display = "none";
-}
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  ValidPhone()
+})
+
+
+const ValidPhone = () => {
+  const regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+  let myPhoneValue = document.getElementById('phone').value;
+  let myPhone = document.getElementById('phone');
+  const message = document.querySelector('.message')
+  const danger = document.querySelector(".formTel__danger")
+
+  let valid = regex.test(myPhoneValue);
+  if (!valid) {
+    message.innerText = 'Проверьте набранный номер';
+    message.style.color = "#363636";
+    myPhone.style.border = "4px solid #BF4444";
+    danger.style.display = "block";
+  } else {
+    message.innerText = 'Контактный телефон';
+    message.style.color = "#DAE0E5";
+    myPhone.style.border = "4px solid #47B247";
+    danger.style.display = "none";
+  }
+  return valid;
+}  
